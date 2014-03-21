b65bc1dc5c8530d06e493abd56fa0d70a75f9bb3
benpious <benpious@gmail.com>
2014-03-21 15:28:59 -0400

    Initial Commit

---
+++ CodeSample/CSSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSSAppDelegate.h
+//  CodeSample
+//
+//  Created by Benjamin Pious on 3/21/14.
+//  Copyright (c) 2014 benpious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CodeSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CodeSample
+//
+//  Created by Benjamin Pious on 3/21/14.
+//  Copyright (c) 2014 benpious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSSAppDelegate class]));
+    }
+}
